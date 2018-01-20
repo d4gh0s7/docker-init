83327b479e168e8d19c2680cbe6e3c1020017fe6
F <f@widememory.com>
2018-01-20 13:51:35 +0200

    Added go-replace to the toolbox

--- init-docker.sh
+++ init-docker.sh
@@ -82,9 +82,8 @@ get_toolbox() {
 	$sh_c "mkdir -p $workdir/go"
 	$sh_c "wget -O $workdir/go/gosync https://raw.githubusercontent.com/d4gh0s7/centos-docker-init/master/toolbox/go/gosync"
 
-	# goreplace https://github.com/webdevops/go-sync/releases
-	$sh_c "mkdir -p $workdir/go"
-	$sh_c "wget -O $workdir/go/go-sync https://raw.githubusercontent.com/d4gh0s7/centos-docker-init/master/toolbox/go/go-sync"
+	# go-replace https://github.com/webdevops/go-replace
+	$sh_c "wget -O $workdir/go/go-replace https://raw.githubusercontent.com/d4gh0s7/centos-docker-init/master/toolbox/go/go-replace"
 }
 
 init_system() {
